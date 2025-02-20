1bbec4d92aab696f2abfaf868547d224e4bdec1f
zhengjunqin <zhengjunqin@shoplazza.com>
2025-02-20 15:28:46 +0800

    modify: 删除文件

--- src/renderer/src/containers/clip/containers/PlayerDomain/index.tsx
+++ src/renderer/src/containers/clip/containers/PlayerDomain/index.tsx
@@ -1,14 +1,8 @@
-import VideoPlayer from './components/VideoPlayer'
-
-interface IDualPlayer {}
-
 const PlayerDomain: React.FC<IDualPlayer> = () => {
   return (
     <div className="rounded-xl flex flex-col bg-[#303030] w-full h-full text-white">
       <div className="p-2 border-b-[#141414] border-b-1">播放器</div>
-      <div className="p-2 flex-1 overflow-hidden">
-        <VideoPlayer />
-      </div>
+      <div className="p-2 flex-1 overflow-hidden"></div>
     </div>
   )
 }
